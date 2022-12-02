57b642bd48064e8ac700b5c55308b431da5f8b52
revantark <revantark@gmail.com>
2022-12-03 00:52:05 +0530

    feat: base

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,8 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@typechain/hardhat')
+require('@nomiclabs/hardhat-ethers')
+require('@nomiclabs/hardhat-waffle')
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
