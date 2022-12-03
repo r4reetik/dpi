56b7fe3f923da9c7dfa243c1e4c8f34e981f0a42
Reetik Rajan <44093575+r4reetik@users.noreply.github.com>
2022-12-03 15:42:56 +0530

    fix: ENS calls

--- app/src/utils/ens.ts
+++ app/src/utils/ens.ts
@@ -1,18 +1,22 @@
-import { getDefaultProvider } from "ethers";
-import truncateEthAddress from "truncate-eth-address";
+import { ethers } from "ethers";
 
-export const getEnsNameFromAddress = async (_mmAddress: string, chainId = 5) => {
-  const provider = getDefaultProvider(chainId);
-  return provider.lookupAddress(_mmAddress);
+const provider = new ethers.providers.InfuraProvider("goerli", "8127a04bb3ea4cd39511696573cefe56");
+
+export const getEnsNameFromAddress = async (metamaskAddress: string, chainId = 5) => {
+  const address = ethers.utils.getAddress(metamaskAddress);
+  const ens = await provider.lookupAddress(address);
+  return ens;
 };
 
-export const getAddressFromEns = async (ens: string, chainId = 5) => {
-  const provider = getDefaultProvider(chainId);
-  return provider.resolveName(ens);
+export const getAddressFromEns = async (ensName: string, chainId = 5) => {
+  const address = await provider.resolveName(ensName);
+  return address;
 };
 
-export const getEnsOrAddress = async (_mmAddress: string | null, chainId = 5) => {
-  const provider = getDefaultProvider(chainId);
-  const hasENS = await provider.lookupAddress(_mmAddress as string);
-  return hasENS ? hasENS : truncateEthAddress(_mmAddress as string);
+export const getEnsOrAddress = (data: string, chainId = 5) => {
+  if (data.includes(".eth")) {
+    return getAddressFromEns(data);
+  } else {
+    return getEnsNameFromAddress(data);
+  }
 };
