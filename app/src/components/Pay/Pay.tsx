7ecc227d145f267dd6f83061fa3283ca3dc59722
Gowtham Chittemsetty <saigowthamchittemsetty@Gowthams-MacBook-Air.local>
2022-12-03 20:04:57 +0530

    feat: UI

--- app/src/components/Pay/Pay.tsx
+++ app/src/components/Pay/Pay.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TokenCard, { TokenType } from "../Token/TokenCard";
-import { UilMoneyInsert, UilQrcodeScan, UilArrowUpRight } from "@iconscout/react-unicons";
+import {  UilHistory, UilQrcodeScan, UilArrowUpRight } from "@iconscout/react-unicons";
 import { Tokens } from "../../constants/Tokens";
 import { usePayTC } from "../../contexts/usePaytc";
 import next from "next";
@@ -17,6 +17,12 @@ const Pay = ({ next }: { next: () => void }) => {
   return (
     <div className='flex flex-col gap-y-1'>
       <div className='flex mt-4 text-center justify-around items-center rounded-2xl md:min-w-[448px] py-8 text-white bg-black-800'>
+        <div className='flex flex-col justify-start items-center'>
+          <div className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
+            <UilArrowUpRight />
+          </div>
+          <div className='mt-1'>Send</div>
+        </div>
         <div className='flex flex-col justify-center items-center'>
           <div className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
             <UilQrcodeScan />
@@ -25,9 +31,9 @@ const Pay = ({ next }: { next: () => void }) => {
         </div>
         <div className='flex flex-col justify-start items-center'>
           <div className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
-            <UilArrowUpRight />
+            < UilHistory />
           </div>
-          <div className='mt-1'>Send</div>
+          <div className='mt-1'>History</div>
         </div>
       </div>
       <div className='flex flex-col mt-4 justify-between align-middle rounded-2xl items-center text-white bg-black-800'>
