92e772797f7a6af85af79c75cccd50105b706f46
Shamoil Arsiwala <shamoilarsiwala16@gmail.com>
2022-12-03 08:53:37 +0530

    chore: add tokens

---
+++ app/src/constants/ChainIdNetwork.ts
@@ -0,0 +1,4 @@
+export const ChainIdToNetwork = {
+  [5]: "Goerli Testnet",
+  [80001]: "Mumbai Testnet",
+};
---
+++ app/src/constants/Tokens.ts
@@ -0,0 +1,72 @@
+const Tokens = {
+  5: [
+    {
+      address: "0x0000000000000000000000000000000000000000",
+      chainId: 5,
+      symbol: "ETH",
+      decimals: 18,
+      name: "ETH",
+      priceUSD: "1286.84",
+      logoURI:
+        "https://static.debank.com/image/token/logo_url/eth/935ae4e4d1d12d59a99717a24f2540b5.png",
+      coinKey: "ETH",
+    },
+    {
+      address: "0xb5B640E6414b6DeF4FC9B3C1EeF373925effeCcF",
+      chainId: 5,
+      symbol: "USDC",
+      decimals: 6,
+      name: "USD Coin",
+      priceUSD: "1",
+      logoURI:
+        "https://static.debank.com/image/coin/logo_url/usdc/e87790bfe0b3f2ea855dc29069b38818.png",
+      coinKey: "USDC",
+    },
+    {
+      address: "0x64ef393b6846114bad71e2cb2ccc3e10736b5716",
+      chainId: 5,
+      symbol: "USDT",
+      decimals: 6,
+      name: "Tether USD",
+      priceUSD: "1",
+      logoURI:
+        "https://static.debank.com/image/coin/logo_url/usdt/23af7472292cb41dc39b3f1146ead0fe.png",
+      coinKey: "USDT",
+    },
+  ],
+  80001: [
+    {
+      address: "0xa6fa4fb5f76172d178d61b04b0ecd319c5d1c0aa",
+      chainId: 80001,
+      symbol: "ETH",
+      decimals: 18,
+      name: "ETH",
+      priceUSD: "1286.84",
+      logoURI:
+        "https://static.debank.com/image/token/logo_url/eth/935ae4e4d1d12d59a99717a24f2540b5.png",
+      coinKey: "ETH",
+    },
+    {
+      address: "0xdA5289fCAAF71d52a80A254da614a192b693e977",
+      chainId: 80001,
+      symbol: "USDC",
+      decimals: 6,
+      name: "USD Coin",
+      priceUSD: "1",
+      logoURI:
+        "https://static.debank.com/image/coin/logo_url/usdc/e87790bfe0b3f2ea855dc29069b38818.png",
+      coinKey: "USDC",
+    },
+    {
+      address: "0xeaBc4b91d9375796AA4F69cC764A4aB509080A58",
+      chainId: 80001,
+      symbol: "USDT",
+      decimals: 6,
+      name: "Tether USD",
+      priceUSD: "1",
+      logoURI:
+        "https://static.debank.com/image/coin/logo_url/usdt/23af7472292cb41dc39b3f1146ead0fe.png",
+      coinKey: "USDT",
+    },
+  ],
+};
