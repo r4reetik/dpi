995ecaaa214e6a229bdeff7b21067f38c1bf0654
Gowtham Chittemsetty <saigowthamchittemsetty@Gowthams-MacBook-Air.local>
2022-12-03 17:44:43 +0530

    feat: pay review

--- app/src/components/Pay/PayReview.tsx
+++ app/src/components/Pay/PayReview.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { TokenType } from '../Token/TokenCard';
+import ContentWrapper from '../wrappers/ContentWrapper';
+import TokenBalanceInput from './TokenBalanceInput';
 
-const PayReview = () => {
+interface PayReviewProps {
+    token : TokenType
+}
+
+const PayReview = ({token}:PayReviewProps) => {
     return (
-        <div className=''>
-
+        <div className='w-full mt-10'>
+          <ContentWrapper label={"You Send"}>
+          <div className='flex flex-col gap-1'>
+            <TokenBalanceInput token = {token}/>
+          </div>
+        </ContentWrapper>
         </div>
     );
 }
+
+export default PayReview
--- app/src/components/Pay/TokenBalanceInput.tsx
+++ app/src/components/Pay/TokenBalanceInput.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import AngleDownCircle from "../icons/AngleDownCircle";
 import { toFixed } from "../../utils/misc";
@@ -7,46 +7,13 @@ import { LoadingIndicator } from "../icons/LoadingIndicator";
 import { TokenType } from "../Token/TokenCard";
 
 interface TokenBalanceInputProps {
-  balance: number;
-  showTokenChange?: boolean;
-  onChange: (v: string) => void;
-  changeToken?: () => void;
-  onMaxClick?: () => void;
-  showMMBalance?: boolean;
-  input?: string;
-  isMax?: boolean;
-  maxDecimals?: number;
-  tokenFiatValue: number;
-  balanceClass?: string;
-  maxBtnClass?: string;
-  token: TokenType;
-  showFiat: boolean;
-  addressTokenBalance: number;
-  isDeposit?: boolean;
-  gas?: number;
-  disabled?: boolean;
-  loading?: boolean;
+    token : TokenType
 }
 
-const TokenBalanceInput = ({
-  showTokenChange = true,
-  changeToken,
-  balance,
-  token,
-  input,
-  maxDecimals = 6,
-  showMMBalance = false,
-  onChange,
-  isMax,
-  tokenFiatValue,
-  balanceClass: balanceClassName = "",
-  maxBtnClass = "",
-  onMaxClick,
-  disabled = false,
-  loading = false,
-}: TokenBalanceInputProps) => {
+const TokenBalanceInput = ({token}:TokenBalanceInputProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const fiatAmount = Number(input) * tokenFiatValue;
+  const [input,setInput] = useState<number | null>();
+
   const handleChange = (e: any) => {
     e.preventDefault();
     const value: string = e.target.value;
@@ -57,16 +24,6 @@ const TokenBalanceInput = ({
         return;
       }
     }
-
-    if (maxDecimals) {
-      const pointIndex = value.indexOf(".");
-      if (pointIndex >= 0 && value.substring(pointIndex + 1).length > maxDecimals) {
-        e.preventDefault();
-        return;
-      }
-    }
-
-    onChange(value);
   };
 
   const isValidNumber = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -85,12 +42,10 @@ const TokenBalanceInput = ({
     return true;
   };
 
-  const handleFocus = () => input === "0" && onChange("");
-  const handlePaste = (e: any) => e.preventDefault();
 
   return (
     <div className='flex flex-col md:min-h-[72px] bg-black-900 md:bg-black-800 rounded-xl'>
-      <div className='flex flex-col px-4 pt-3 pb-1 sm:items-center sm:justify-between sm:flex-row '>
+      <div className='flex justify-between gap-4 px-4 pt-3 pb-1 sm:items-center sm:justify-between sm:flex-row '>
         <div className='flex items-center'>
           <div className='w-10 h-10 mr-4 pointer-events-none select-none'>
             <Image
@@ -104,75 +59,50 @@ const TokenBalanceInput = ({
           <div>
             <div className='flex items-center font-bold'>
               <span className='tracking-wide md:text-lg'>{token.symbol}</span>
-              {showTokenChange && (
-                <button onClick={changeToken}>
+                <button onClick={()=>{}}>
                   <AngleDownCircle className='ml-2' />
                 </button>
-              )}
             </div>
 
             <p
-              className={`font-semibold flex justify-center text-grey-500 text-sm tracking-wide ${balanceClassName}`}>
-              {showMMBalance && `${"Balance"}: ${
-                    !isNaN(balance) ? toFixed(balance) : 'Loading...'
-                  }`}
+              className={`font-semibold flex justify-center text-grey-500 text-sm tracking-wide`}>
+              {`${"Balance"}: ${ ''
+}`}
             </p>
           </div>
         </div>
         <div className='sm:w-[216px] p-0 border-0 inline-flex flex-col relative mt-2 md:mt-0'>
           <div
-            className={`py-2 relative inline-flex font-[inherit] ${loading ? "opacity-50" : ""}`}>
+            className={`py-2 relative inline-flex font-[inherit] "opacity-50"}`}>
             <input
               type='number'
               inputMode='decimal'
               ref={inputRef}
               className='peer text-left md:text-right w-full border-none outline-none select-none font-[inherit] text-current bg-inherit font-bold hide-arrows'
-              value={inputToFixed(input ?? "")}
               onWheel={(e) => {
                 e.currentTarget.blur();
                 e.stopPropagation();
               }}
               step='any'
               onChange={handleChange}
-              onKeyPress={isValidNumber}
-              onPaste={handlePaste}
-              onFocus={handleFocus}
-              disabled={isNaN(balance) || disabled}
               min={0}
-              placeholder={isNaN(balance) ? `${"Fetching values" + "..."}` : ""}
+              placeholder={`Enter Amount here`}
             />
-            {loading ? (
-              <div>
+              {/* <div>
                 <LoadingIndicator />
-              </div>
-            ) : null}
+              </div> */}
             <fieldset
               aria-hidden='true'
               className='absolute inset-0 px-2 py-0 m-0 overflow-hidden transition-colors border-b-2 border-solid rounded-sm pointer-events-none border-b-black-600 peer-focus:border-b-primary'></fieldset>
-            {onMaxClick && (
-              <div className='flex items-center justify-center ml-2 '>
-                <button
-                  disabled={isNaN(balance)}
-                  className={`ml-2 text-xs tracking-wide bg-black-900 rounded-md border-solid border-2 px-1 font-semibold ${
-                    isMax || isNaN(balance)
-                      ? "text-black-600 cursor-default border-black-600"
-                      : "border-primary text-primary"
-                  } ${maxBtnClass}`}
-                  onClick={onMaxClick}>
-                  MAX
-                </button>
-              </div>
-            )}
           </div>
         </div>
       </div>
-      {fiatAmount !== 0 ? (
+      {/* {fiatAmount !== 0 ? (
         <span className='self-end mb-2 mr-4 text-sm font-semibold text-[#c3c3c4] '>
           {`~ $${fiatAmount.toFixed(2)}`}
-        </span>
-      ) : (
+        </span> */}
+      {/* ) : ( */}
         <div className='mb-2' />
-      )}
     </div>
   );
 };
