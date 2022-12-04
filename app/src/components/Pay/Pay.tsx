3d42a02b27b6e07f60d593ef07016ba44f45338b
revantark <revantark@gmail.com>
2022-12-04 08:52:14 +0530

    feat: add btc

--- app/src/components/Pay/Pay.tsx
+++ app/src/components/Pay/Pay.tsx
@@ -6,30 +6,42 @@ import { TokenType } from "../../constants/Tokens";
 import { usePayTC } from "../../contexts/usePaytc";
 import { PageType } from "../../pages";
 
-
-const Pay = ({ next,setPage }: { next: () => void,setPage:Dispatch<SetStateAction<PageType>> }) => {
-  const { setSelectedToken,tokens } = usePayTC();
+const Pay = ({
+  next,
+  setPage,
+}: {
+  next: () => void;
+  setPage: Dispatch<SetStateAction<PageType>>;
+}) => {
+  const { setSelectedToken, tokens } = usePayTC();
   const handleTokenClick = async (_token: TokenType) => {
     setSelectedToken(_token);
+    setPage("btcDeposit");
     next();
   };
 
   return (
     <div className='flex w-full flex-col gap-y-1'>
       <div className='flex mt-4 text-center justify-around items-center rounded-2xl md:min-w-[448px] py-8 text-white bg-black-800'>
-        <div onClick={()=>setPage("amountRecipient")} className='flex flex-col justify-start items-center'>
-          <div  className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
+        <div
+          onClick={() => setPage("amountRecipient")}
+          className='flex flex-col justify-start items-center'>
+          <div className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
             <UilArrowUpRight />
           </div>
           <div className='mt-1'>Send</div>
         </div>
-        <div onClick={()=>setPage("inputRecipient")} className='flex flex-col justify-center items-center'>
+        <div
+          onClick={() => setPage("inputRecipient")}
+          className='flex flex-col justify-center items-center'>
           <div className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
             <UilQrcodeScan />
           </div>
           <div className='mt-1'>Scan & Pay</div>
         </div>
-        <div onClick={()=>setPage("history")} className='flex flex-col justify-start items-center'>
+        <div
+          onClick={() => setPage("history")}
+          className='flex flex-col justify-start items-center'>
           <div className='flex justify-center items-center font-semibold rounded-full w-16 h-16 bg-primary'>
             <UilHistory />
           </div>
@@ -42,6 +54,35 @@ const Pay = ({ next,setPage }: { next: () => void,setPage:Dispatch<SetStateActio
         </div>
         <div className='w-full p-2'>
           <div className='flex flex-col gap-0.5 mb-2 justify-start'>
+            <>
+              <TokenCard
+                key={"gfhvjbkn"}
+                token={{
+                  decimals: 8,
+                  domainId: "1",
+                  symbol: "BTC",
+                  address: "0x880Ad65DC5B3F33123382416351Eef98B4aAd7F1",
+                  chain: "Ethereum",
+                  chainId: 5,
+                  priceUSD: 16976,
+                  image:
+                    "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png",
+                }}
+                onClick={() =>
+                  handleTokenClick({
+                    decimals: 8,
+                    domainId: "1",
+                    symbol: "BTC",
+                    address: "0x880Ad65DC5B3F33123382416351Eef98B4aAd7F1",
+                    chain: "Ethereum",
+                    chainId: 5,
+                    priceUSD: 16976,
+                    image:
+                      "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/1024/Bitcoin-BTC-icon.png",
+                  })
+                }
+              />
+            </>
             {Object.values(tokens).map((_token) => (
               <TokenCard
                 key={`${_token.chain}-${_token.symbol}`}
